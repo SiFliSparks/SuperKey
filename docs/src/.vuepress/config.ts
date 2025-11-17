b321f96d7f6564a74e94a7d722e36a831b20fd81
bowenxie <bowenxie@sifli.com>
2025-11-17 12:06:35 +0800

    ci for doc

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/projects/xiaozhi/",
+  base: "/projects/superkey/",
 
   locales: {
     "/en/": {
